af856c0f696f221212c8c20aef7041fa8ee7baec
mehmetKadiOglu <37665451+mehmetKadiOglu@users.noreply.github.com>
2019-11-22 16:12:19 +0300

    Add files via upload

---
+++ 2/constrainedProblems/penalty/constrainedChemistryP4.m
@@ -0,0 +1,12 @@
+function p = constrainedChemistryP4(x)
+    p = 0;
+    c1 = -10;
+    for index = 1:5
+        c1 = c1 + x(index)^2;
+    end
+    c2 = x(2)*x(3) - 5*x(4)*x(5);
+    c3 = x(1)^3 + x(2)^3 + 1;
+    if ( c1 ~= 0 || c2 ~= 0 || c3 ~= 0 )
+        p = inf;
+    end
+end
