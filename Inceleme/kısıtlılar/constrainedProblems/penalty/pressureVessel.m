09ccf01012da66f045a19f7931317d1c53e94fa7
mehmet kati <mehmet_kadi@outlook.com>
2019-12-22 22:12:37 +0300

    guncelleme 22.12.2019

--- Inceleme/kısıtlılar/constrainedProblems/penalty/pressureVessel.m
+++
@@ -1,9 +0,0 @@
-function p = pressureVessel(x)
-    
-    c1 = 0.0193 * x(3) - x(1);
-    c2 = 0.00954 * x(3) - x(2);
-    c3 = 1296000 - (pi * x(3)^2 * x(4)) - ((4 * pi * x(3)^3) / 3);
-    c4 = x(4) - 240;
-    p = get_penalty([c1, c2, c3, c4]);
-    
-end
