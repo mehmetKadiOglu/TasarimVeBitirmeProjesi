0d89e1b85fbddb5df49fad6a09f418f948343d3e
mehmetKadiOglu <37665451+mehmetKadiOglu@users.noreply.github.com>
2020-01-03 15:39:38 +0300

    Add files via upload

---
+++ Inceleme/atilacak/Kafka/demo/src/main/java/com/example/demo/kafkatopicconfig.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value(value = "localhost:9095")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic1() {
+        return new NewTopic("deneme5", 1, (short) 1);
+    }
+}
